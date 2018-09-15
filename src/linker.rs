e600a7e739f69d898ab4a027cf9ca652ff986420
dylan_DPC <dylan.dpc@gmail.com>
2018-09-16 00:51:04 +0530

    scrape now returns a LinksList instead of Vec. Links are now in proper format

--- src/linker.rs
+++ src/linker.rs
@@ -1,6 +1,7 @@
 use reqwest::{Url, Response, get};
 use failure::Error;
 use scraper::{Html, Selector};
+use std::ops::Deref;
 
 pub struct Scraper {
     pub path: Url
@@ -11,7 +12,7 @@ impl Scraper {
         Ok(Scraper { path: Url::parse(path)? })
     }
 
-    pub fn scrape(self, selector: &str) -> Result<Vec<String>, Error> {
+    pub fn scrape(self, selector: &str) -> Result<LinksList, Error> {
         let body = get(self.path.as_str())?.text()?;
         let document = Html::parse_document(&body);
         let picker = Selector::parse(".module-item").unwrap();
@@ -23,11 +24,23 @@ impl Scraper {
             let attribute_pred: &[_] = &['h', 'r', 'e', 'f', '=', '"'];
             let links_list = attributes.trim_matches(pred).split(' ')
                 .filter(|x| x. contains("href"))
-                .map(|x| x.trim_matches(attribute_pred).to_owned())
+                .map(|x| x.trim_matches(attribute_pred).splitn(2, '\"').take(1).collect::<String>())
                 .for_each(|x| text.push(x));
         }
-        Ok(text)
+        Ok(LinksList(text))
     }
+}
+
+#[derive(Clone, Debug)]
+pub struct LinksList(pub Vec<String> );
+
+impl Deref for LinksList {
+    type Target = Vec<String>;
+
+    fn deref(&self) -> &Vec<String> {
+        &self.0
+    }
+
 }
 
 #[cfg(test)]
@@ -36,5 +49,6 @@ mod tests {
     #[test]
     fn test_scrape() {
          let results = Scraper::from("https://docs.rs/uuid/0.7.0/uuid/").unwrap().scrape("");
+        println!("{:?}", results );
     }
 }
